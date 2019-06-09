25b15d6dd06afd7ca70b2844e8152238b55f57e5
yansheng <yeasheng@126.com>
2019-06-09 19:54:24 +0800

    第一次新建项目

---
+++ src/main/java/com/yeasheng/yeashengmall/YeashengmallApplication.java
@@ -0,0 +1,13 @@
+package com.yeasheng.yeashengmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeashengmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeashengmallApplication.class, args);
+	}
+
+}
