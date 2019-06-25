9723f7304f306db806d03e9413c2eb3a330a8699
yansheng <yeasheng@126.com>
2019-06-25 21:51:21 +0800

    新增Redis配置

--- mall-demo/src/main/java/com/yeasheng/malldemo/controller/MemberController.java
+++ mall-demo/src/main/java/com/yeasheng/malldemo/controller/MemberController.java
@@ -48,7 +48,7 @@ public class MemberController {
 			copyNonNullProperties(user, existing);*/
 			MemberEntity detail = memberDao.findById(id)
 					.orElseThrow(() -> new ResourceNotFoundException("MemberEntity", "id", id));
-			copyNonNullProperties(param,detail);
+			copyNonNullProperties(param, detail);
 			result = memberDao.save(detail);
 		} catch (ResourceNotFoundException e) {
 			e.printStackTrace();
@@ -68,23 +68,29 @@ public class MemberController {
 		return memberDao.findById(id).orElseThrow(() -> new ResourceNotFoundException("MemberEntity", "id", id));
 	}
 
+
+
 	private class ResourceNotFoundException extends Exception {
 
 		private ResourceNotFoundException(String memberEntity, String id, Integer id1) {
 			super.getMessage();
 		}
 	}
+
 	public static void copyNonNullProperties(Object src, Object target) {
 		BeanUtils.copyProperties(src, target, getNullPropertyNames(src));
 	}
-	public static String[] getNullPropertyNames (Object source) {
+
+	public static String[] getNullPropertyNames(Object source) {
 		final BeanWrapper src = new BeanWrapperImpl(source);
 		java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
 
 		Set<String> emptyNames = new HashSet<String>();
-		for(java.beans.PropertyDescriptor pd : pds) {
+		for (java.beans.PropertyDescriptor pd : pds) {
 			Object srcValue = src.getPropertyValue(pd.getName());
-			if (srcValue == null) emptyNames.add(pd.getName());
+			if (srcValue == null) {
+				emptyNames.add(pd.getName());
+			}
 		}
 		String[] result = new String[emptyNames.size()];
 		return emptyNames.toArray(result);
--- mall-demo/src/main/java/com/yeasheng/malldemo/repos/MemberDao.java
+++ mall-demo/src/main/java/com/yeasheng/malldemo/repos/MemberDao.java
@@ -2,12 +2,14 @@ package com.yeasheng.malldemo.repos;
 
 import com.yeasheng.malldemo.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Desc:
  * @author: yansheng
  * @date: 2019/6/16
  */
+@Repository
 public interface MemberDao extends JpaRepository<MemberEntity,Integer>{
 }
 
